48ec2c9304f732cafd0dd98bb94de9e42c2c6afa
collins njau <collinsnjau39@gmail.com>
2019-12-09 20:16:20 +0300

    Add spider to scrap the website

---
+++ AroSpider/AroSpider/spiders/horse.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class HorseSpider(scrapy.Spider):
+
+  #give it a unique name so that scrappy can know which spider to run
+  name = 'tarantula'
+
+  # defines initial request to be made
+  # how to follow links
+  def start_requests(self):
+    # list of urls to process
+    urls = ['https://treehouse-projects.github.io/horse-land/index.html', 'https://treehouse-projects.github.io/horse-land/mustang.html']
+
+    return [scrapy.Request(url=url, callback=self.parse) for url in urls]
+  
+  # tell the spider how extracted data will be parsed
+  def parse(self, response):
+    url = response.url # rep a response object from the request made in start_request
+    page =  url.split('/')[-1]
+    filename= 'horses-%s' % page
+    print('URL is: {}'.format(url))
+    with open(filename, 'wb') as file:
+      file.write(response.body)
+    print('Saved file %s' % filename)
